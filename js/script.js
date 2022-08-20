498bdc67abb14fd1c51155f5701311ac4434ed32
jesuspaz5000 <jpazl@gmail.com>
2022-08-20 17:05:32 -0500

    implementacion del search

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 let navbar = document.querySelector('.header .navbar');
+let searchBtn = document.querySelector('#search-btn');
+let searchForm = document.querySelector('.header .search-form');
+
+searchBtn.onclick = () =>{
+    searchBtn.classList.toggle('fa-times');
+    searchForm.classList.toggle('active');
+};
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
